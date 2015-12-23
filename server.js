50590b280ba0860cad6b138c6931e64e8e13fdb6
chwex <chwex@hotmail.com>
2015-12-22 23:48:08 -0700

    changed to express

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-var http = require('http'),
-    fs = require('fs');
 
-http.createServer(function(req, res){
-   res.writeHead(200,{
-      'Content-Type':'text/html',
-      'Access-Control-Allow-Origin':'*' 
-   }); 
-   
-   var readStream = fs.createReadStream(__dirname  + '/index.html');
-   
-   readStream.pipe(res);
-}).listen(1337);
+var express = require('express');
+var app = express();
+var path = require('path');
 
-console.log("Visit me at http://localhost:1337")
+app.get('/', function(req, res){
+   res.sendFile(path.join(__dirname + '/index.html')); 
+});
+
+app.listen(1337);
+console.log('1337 is the magic port');
