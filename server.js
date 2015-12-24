08edf3200bc90874689c60e173efb3e00cd805f3
chwex <chwex@hotmail.com>
2015-12-24 11:56:37 -0700

    copy route

--- server.js
+++ server.js
@@ -39,16 +39,25 @@ adminRouter.get('/users/:name', function(req, res){
     res.send('hello '+req.params.name+'!');
 });
 
-adminRouter.param('name', function(req, res, next, name){
-    console.log('doing name validation on '+name);
-    
-    req.name = name;
-    next();
-});
+ // route middleware to validate :name
+ adminRouter.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // blah blah validation
+    // log something so we know its working
+     console.log('doing name validations on ' + name);
 
-adminRouter.get('/hello/:name', function(req, res){
-    res.send('hello '+req.name+'!');
-});
+    // once validation is done save the new item in the req
+     req.name = name;
+    // go to the next thing
+    next();
+ });
+
+ // route with parameters (http://localhost:1337/admin/hello/:name)
+ adminRouter.get('/hello/:name', function(req, res) {
+     res.send('hello ' + req.name + '!');
+ });
+
+
 
 
 
